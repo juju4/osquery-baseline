a82b000e0ddb2dd4c84731df7d752827d5b5c1ef
juju4 <juju4@users.noreply.github.com>
2018-08-25 16:17:52 -0400

    osquery process: update with 2 instances

--- controls/osquery.rb
+++ controls/osquery.rb
@@ -60,8 +60,8 @@ control 'osquery-2.0' do
   title 'Osqueryd should be running'
   desc 'Ensure osqueryd is running'
   describe processes('osqueryd') do
-    its('users') { should eq ['root'] }
-    its('list.length') { should eq 1 }
+    its('users') { should eq ['root', 'root' ] }
+    its('list.length') { should eq 2 }
   end
 end
 
